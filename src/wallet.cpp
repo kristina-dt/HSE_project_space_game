430dd1617f382cc30bcada41026a443069a797c2
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-10 22:49:49 +0300

    add method of withdraw in wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1,8 +1,16 @@
 
 #include "../include/wallet.h"
-
+#include <string>
+#include "../include/NotEnoughMoney.h"
 wallet::wallet(int initialAmount) noexcept : balance(initialAmount) {
     if (initialAmount<0) {
         balance = 0;
     }
+}
+void wallet::withdraw(int amount) {
+    if (amount > balance) {
+        throw NotEnoughMoney( "Not enough money. Need:" + std::to_string(amount) +
+            ", available: " + std::to_string(balance));
+    }
+    balance -= amount;
 }
