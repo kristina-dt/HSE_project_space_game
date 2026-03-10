d187e05ae9a68e646268761b22a4cba6af4595ff
shudjixx <yugreta2008@gmail.com>
2026-03-11 00:33:30 +0300

    Add class Ship

---
+++ include/Ship.h
@@ -0,0 +1,13 @@
+//
+// Created by Greta Yugay on 11.03.2026.
+//
+
+#ifndef HSE_PROJECT_SPACE_GAME_SHIP_H
+#define HSE_PROJECT_SPACE_GAME_SHIP_H
+
+
+class Ship {
+};
+
+
+#endif //HSE_PROJECT_SPACE_GAME_SHIP_H
