48d461eb32da09766d0c66109eef8d7f3351e7e9
kristina-dt <christi.dt23@gmail.com>
2026-03-11 01:16:29 +0300

    changing the GameEngine.h

--- include/GameEngine.h
+++ include/GameEngine.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "Map.h"
 
 class GameEngine {
+    std::vector<std::string> map;
+    bool keepRunning;
+    const int WIDTH = 30;
+    const int HEIGHT = 13;
+    const float CELL_SIZE = 32.0f;
+public:
+    GameEngine();
+    void run(InformationPlayer& player);
+    void movements(InformationPlayer& player);
 };
 
