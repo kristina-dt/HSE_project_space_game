fcd8885a8c07fa40edda64b00efb6e9f717b1d22
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-10 22:22:20 +0300

    add classes wallet

---
+++ include/wallet.h
@@ -0,0 +1,13 @@
+//
+// Created by Arina on 10.03.2026.
+//
+
+#ifndef HSE_PROJECT_SPACE_GAME_WALLET_H
+#define HSE_PROJECT_SPACE_GAME_WALLET_H
+
+
+class wallet {
+};
+
+
+#endif //HSE_PROJECT_SPACE_GAME_WALLET_H
