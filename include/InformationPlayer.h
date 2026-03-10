5b9e84b317bca9e0ded361ef3e087c49e139f048
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-11 00:14:13 +0300

    add balance, methods: getRecourse and sellPrice in InformationPlayer.h

--- include/InformationPlayer.h
+++ include/InformationPlayer.h
@@ -13,15 +13,17 @@ private:
     std::string name;
     std::vector<Resource> inventory;
     int posX, posY;
-    wallet wallet;
+    wallet wall;
     std::map<Resource::Type, int> sellPrice;
 public:
     InformationPlayer (const std::string namepla, int py, int px, int startMoney);
     std::string getName()const;
     int getX()const{return posX;};
     int getY()const{return posY;};
-
-
+    wallet& getWal();
+    int getAmountResource(Resource::Type type) const;
+    int getSellPrice(Resource::Type type) const;
+    void print() const;
 };
 
 
