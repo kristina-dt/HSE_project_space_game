243b55e6e0a54e45d8967405ef3cead605329d62
kristina-dt <christi.dt23@gmail.com>
2026-03-11 00:00:55 +0300

    add WholeMap method

--- include/Map.h
+++ include/Map.h
@@ -1,3 +1,8 @@
 #pragma once
+#include "InformationPlayer.h"
+#include <vector>
+#include <string>
 class Map {
+public:
+    static void WholeMap(const std::vector<std::string>& map, const InformationPlayer& player);
 };
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,1 +1,18 @@
 #include "../include/Map.h"
+#include "GameEngine.h"
+#include <iostream>
+
+void Map::WholeMap(const std::vector<std::string> &map, const InformationPlayer& player) {
+    for (int i = 0; i < GameEngine::HEIGHT; ++i) {
+        for (int j = 0; j < GameEngine::WIDTH; ++j) {
+            if (i==(player.getY()) && j==(player.getX())) {
+                std::cout<<"[^]";
+                j+=2;
+            }
+            if (j < GameEngine::WIDTH) {
+                std::cout<<map[i][j];
+            }
+        }
+        std::cout<<'\n';
+    }
+}
