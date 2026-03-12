187762d072bd5742d2dab5f853db4303dcb9b0f9
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-12 13:21:20 +0300

    add constructor in test_InfoPlayer.cpp

--- tests/test_InfoPlayer.cpp
+++ tests/test_InfoPlayer.cpp
@@ -1,3 +1,15 @@
 #include <gtest/gtest.h>
 #include <InformationPlayer.h>
 
+TEST(InformationPlayerTest, Constructor) {
+    InformationPlayer player("TestPlayer", 10.5f, 20.5f);
+    EXPECT_EQ(player.getName(), "TestPlayer");
+    EXPECT_EQ(player.getX(), 10.5f);
+    EXPECT_EQ(player.getY(), 20.5f);
+    EXPECT_EQ(player.getWal().getBal(), 100);
+    EXPECT_EQ(player.getAmountResource(Resource::Type::Fuel), 5);
+    EXPECT_EQ(player.getAmountResource(Resource::Type::Food), 7);
+    EXPECT_EQ(player.getAmountResource(Resource::Type::Drinks), 7);
+    EXPECT_EQ(player.getAmountResource(Resource::Type::Details), 5);
+    EXPECT_EQ(player.getAmountResource(Resource::Type::Decorations), 0);
+}
