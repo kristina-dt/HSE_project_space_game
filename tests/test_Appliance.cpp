1a556997d9061e15841337275b90c69a2ecfabf6
vitchk <vorobevavioletta971@gmail.com>
2026-03-12 14:32:09 +0300

    Correct GoogleTest for Appliance

--- tests/test_Appliance.cpp
+++ tests/test_Appliance.cpp
@@ -14,31 +14,3 @@ TEST(PositionTest, IsNearby) {
     EXPECT_FALSE(pos.isNearby(8, 5, 2.0f));
     EXPECT_TRUE(pos.isNearby(10, 5, 5.0f));
 }
-
-TEST(ApplianceFromCppTest, Upgrade) {
-    class TestAppliance : public Appliance {
-    public:
-        TestAppliance(std::string name, int x, int y) {
-            name_ = name;
-            position_ = Position(x, y);
-        }
-        std::string getType() const override { return "Test"; }
-        bool produce(InformationPlayer&) override { return true; }
-        int getUpgradeCost() const override { return 100; }
-        int getCurrentPrice() const override { return 50; }
-    };
-
-    TestAppliance app("Test", 0, 0);
-
-    EXPECT_EQ(app.getLevel(), 1);
-
-    testing::internal::CaptureStdout();
-    app.upgrade();
-    std::string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ(app.getLevel(), 2);
-    EXPECT_EQ(output, "Test upgraded to level 2!\n");
-
-    app.upgrade();
-    EXPECT_EQ(app.getLevel(), 3);
-}
