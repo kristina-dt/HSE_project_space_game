b28e52a6c6ba8ef7054849e8f0867f5fe4e6b769
arinchikzhuzhik <arinazhuzhoma4@gmail.com>
2026-03-12 12:40:03 +0300

    add funcs of money in test_wall.cpp

--- tests/test_wall.cpp
+++ tests/test_wall.cpp
@@ -1,7 +1,32 @@
 #include <gtest/gtest.h>
 #include "wallet.h"
-
+TEST(WalletTest, ConstructorWithPositiveAmount) {
+    wallet w(100);
+    EXPECT_EQ(w.getBal(), 100);
+}
+TEST(WalletTest, ConstructorWithZeroAmount) {
+    wallet w(0);
+    EXPECT_EQ(w.getBal(), 0);
+}
 TEST(WalletTest, ConstructorWithNegativeAmount) {
     wallet w(-50);
     EXPECT_EQ(w.getBal(), 0);
+}
+TEST(WalletTest, DepositMoney) {
+    wallet w(100);
+    w.deposit(50);
+    EXPECT_EQ(w.getBal(), 150);
+
+    w.deposit(25);
+    EXPECT_EQ(w.getBal(), 175);
+}
+TEST(WalletTest, DepositFromZero) {
+    wallet w(0);
+    w.deposit(200);
+    EXPECT_EQ(w.getBal(), 200);
+}
+TEST(WalletTest, WithdrawMoney) {
+    wallet w(100);
+    w.withdraw(40);
+    EXPECT_EQ(w.getBal(), 60);
 }
