5cac22590c240cbd99488f270ec6223c736458ff
vitchk <vorobevavioletta971@gmail.com>
2026-03-13 16:49:26 +0300

    Change function upgradeStation

--- src/FoodMaker.cpp
+++ src/FoodMaker.cpp
@@ -63,7 +63,7 @@ bool Foodmaker::produce(InformationPlayer& player) {
 
     player.addResource(resourceType, 1);
 
-    std::cout << "🍳 " << name_ << " (Lvl " << level_ << ") produced 1 " << typeName << "\n";
+    std::cout  << name_ << " (Lvl " << level_ << ") produced 1 " << typeName << "\n";
     std::cout << "   Price: " << price << " credits\n";
     std::cout << "   Money left: " << wallet.getBal() << "\n";
     std::cout << "   Total " << typeName << ": "
@@ -78,7 +78,7 @@ int Foodmaker::getUpgradeCost() const {
 
 void Foodmaker::showAvailableModes() {
     std::cout << "\n=== Available Modes ===\n";
-    std::cout << "0. Food\n";
+    std::cout << "0. Food\мn";
     std::cout << "1. Drinks\n";
     std::cout << "=======================\n";
 }
