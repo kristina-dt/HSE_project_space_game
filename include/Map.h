2a2c7f3e84aad09b5d56bd6072d619c4d45027f8
kristina-dt <christi.dt23@gmail.com>
2026-03-13 21:37:53 +0300

    add a start window and text about the food station

--- include/Map.h
+++ include/Map.h
@@ -5,10 +5,23 @@
 #include "InformationPlayer.h"
 #include "Ship.h"
 #include "Appliance.h"
+#include "Foodmaker.h"
+
+enum class WindowType { Food, Info, Warning };
+
+enum class FoodMakerWindow { Food, Drinks, Level, CurrentPrice, Name };
+
 class Map {
     sf::Font font;
+    float cellSize = 28.0f;
+    sf::RectangleShape text_window;
+    sf::Text text;
+    FoodMakerWindow m_currentFoodMode = FoodMakerWindow::Name;
+    Foodmaker foodmaker;
 public:
-    bool loadResources();
+    Map();
+    //bool loadResources();
     static void draw(sf::RenderWindow& window, const std::vector<std::string>& map, const InformationPlayer& player, const std::vector<Ship>& ships);
     void drawWindow(sf::RenderWindow &window);
+    void prepareWindow(WindowType type);
 };
---
+++ include/StartScreen.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <sstream>
+#include <string>
+
+class StartScreen {
+    sf::Text def_text;
+    sf::Font font;
+    std::ostringstream text;
+    sf::RectangleShape textbox;
+    int limit;
+
+    void deleteChar();
+
+public:
+    StartScreen(sf::Font &font);
+    void setFont(sf::Font& font);
+    void setPosition(sf::Vector2f pos);
+    std::string getText();
+    void drawTo(sf::RenderWindow& window);
+    void typed(const sf::Event& input);
+};
+
